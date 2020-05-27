ac87306bf6fea8e37b93e8c227b84888ffc38d62
Chris Camplin <chris@signal-noise.co.uk>
2020-05-27 19:06:13 +0100

    Updates

---
+++ precache-manifest.25920f3230eef238e6a74d3d0fc57bc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e3a35792a2a60b9dad2351f794d38d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fae4a01b844be68ebe94",
+    "url": "/static/css/main.ac2359c6.chunk.css"
+  },
+  {
+    "revision": "425b426a2e690fd20ca5",
+    "url": "/static/js/2.49060a5c.chunk.js"
+  },
+  {
+    "revision": "ca23bffbe249e3ea6ce9ce0e34d79d77",
+    "url": "/static/js/2.49060a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae4a01b844be68ebe94",
+    "url": "/static/js/main.cc09e525.chunk.js"
+  },
+  {
+    "revision": "1d174dd0b0ae47ee00c0",
+    "url": "/static/js/runtime-main.60707d35.js"
+  }
+]);
